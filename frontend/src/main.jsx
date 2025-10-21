3fd9f289e7620bb9ef7a5ccba5645c884074039a
HarshaNaidu11 <harshavardhannaidu1111@gmail.com>
2025-10-22 00:23:11 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,10 +8,10 @@ import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> {/* Added StrictMode back for stability */}
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider> 
         <App /> 
       </AuthProvider>
-    </BrowserRouter>
+   </HashRouter>
   </React.StrictMode>
 );
